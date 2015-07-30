4f3897667968ffdf0f1bab59c047dca3148aa29c
aleaf <andrew.t.leaf@gmail.com>
2015-07-30 15:52:40 -0500

    working on basemap

--- Figures/report_figs.py
+++ Figures/report_figs.py
@@ -1,8 +1,17 @@
 __author__ = 'aleaf'
 
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+try:
+    from mpl_toolkits.basemap import Basemap, pyproj
+    import fiona
+    from GISio import get_proj4
+except:
+    pass
 import textwrap
+
 '''
 try:
     import seaborn as sb
@@ -70,7 +79,7 @@ class ReportFigures(object):
 
         pass
 
-    def figure_title(self, ax, title, zorder=200, wrap=50,
+    def title(self, ax, title, zorder=200, wrap=50,
                      subplot_prefix='',
                      capitalize=True):
 
@@ -271,3 +280,200 @@ class ReportFigures(object):
         set_palette : set the default color palette for figures
 
         """
+
+class basemap:
+
+    projection = 'tmerc'
+    radii = {'NAD83': (6378137.00, 6356752.314245179),
+             'NAD27': (6378206.4, 6356583.8)}
+
+    graticule_text_color = 'k'
+    graticule_label_style = '+/-'
+
+    Basemap_kwargs = {'projection': 'tmerc',
+                      'lon_0': -93,
+                      'lat_0': 0,
+                      'lat_1':None,
+                      'lat_2': None,
+                      'lon_1': None,
+                      'lon_2': None,
+                      'k_0': None,
+                      'resolution': 'i'}
+
+    def __init__(self, ax, extent, proj4=None, epsg=None, datum='NAD27', shapefile=None,
+                 tick_interval=0.2,
+                  yticklabels=[True, False, True, False], xticklabels=[True, False, True, False],
+                  subplots=False,
+                  scalebar_loc='centered', scalebar_color='k', scalebar_pad=0.08, **kwargs):
+
+        self.ax = ax
+        self.extent_proj = extent
+        self.proj4 = proj4
+        self.epsg = epsg
+        self.datum = datum
+
+        if shapefile is not None:
+            self._read_shapefile_proction(shapefile)
+            self.proj4 = get_proj4(shapefile)
+
+        self._convert_map_extent()
+
+        self.Basemap_kwargs['rsphere'] = self.radii[self.datum]
+        self.Basemap_kwargs.update(kwargs)
+
+
+        '''
+        lon_0 is the same as the Central_Meridian in ArcMap (see Layer Properties dialogue)
+        lat_0 is same as Latitude_Of_Origin
+        llcrnrlon, llcrnrlat, etc. all need to be specified in lat/lon
+
+        Note: the major and minor axis radii for the rsphere argument are 6378137.00,6356752.314245179 for NAD83.
+        '''
+        self.m = Basemap(ax=ax, **self.Basemap_kwargs)
+
+        # labels option controls drawing of labels for parallels and meridians (l,r,t,b)
+        self.yticks = self.m.drawparallels(np.arange(self.lat_0 +.15, self.lat_1, tick_interval), linewidth=0, labels=yticklabels,
+                            labelstyle=self.graticule_label_style, rotation=90., color='0.85', size=8, ax=ax)
+        self.xticks = self.m.drawmeridians(np.arange(self.lon_0, self.lon_1, tick_interval), linewidth=0, labels=xticklabels,
+                            labelstyle=self.graticule_label_style, color='0.85', size=8, ax=ax)
+
+        # add tickmarks for the graticules
+        self.add_graticule_ticks()
+
+        # add custom scalebar to plot (for subplots, scalebar is added only to last subplot)
+        #if not subplots:
+        #    add_scalebar(m, ax, scalebar_params, loc=scalebar_loc, color=scalebar_color, scalebar_pad=scalebar_pad)
+
+    def _convert_map_extent(self):
+
+        # convert map units to lat / lons using pyproj
+        if self.epsg is not None:
+            p1 = pyproj.Proj("+init=EPSG:{}".format(self.epsg))
+        else:
+            p1 = pyproj.Proj(self.proj4)
+
+        extent = self.extent_proj
+        self.lon_0, self.lat_0 = p1(extent[0], extent[2], inverse=True)
+        self.lon_1, self.lat_1 = p1(extent[1], extent[3], inverse=True)
+
+        self.Basemap_kwargs.update({'llcrnrlon': self.lon_0,
+                                    'llcrnrlat': self.lat_0,
+                                    'urcrnrlon': self.lon_1,
+                                    'urcrnrlat': self.lat_1})
+
+    def _read_shapefile_proction(self, shapefile):
+
+        crs = fiona.open(shapefile).crs
+
+        self.datum = crs.get('datum', self.datum)
+        self.Basemap_kwargs['projection'] = crs.get('proj', self.Basemap_kwargs['projection'])
+
+        '''
+        {u'datum': u'NAD83',
+         u'lat_0': 23,
+         u'lat_1': 29.5,
+         u'lat_2': 45.5,
+         u'lon_0': -96,
+         u'no_defs': True,
+         u'proj': u'aea',
+         u'units': u'm',
+         u'x_0': 0,
+         u'y_0': 0}
+         '''
+
+    def add_graticule_ticks(self):
+
+        # add ticks for the graticules (basemap only gives option of lat/lon lines)
+        # x and y are instances of drawmeridians and drawparallels objects in basemap
+        x, y = self.xticks, self.yticks
+        # get the vertices of the lines where they intersect the axes by plumbing the dictionaries y, x, obtained above
+        ylpts = [y[lat][0][0].get_data()[1][np.argmin(abs(y[lat][0][0].get_data()[0] - 0))] for lat in y.keys()]
+        yrpts = [y[lat][0][0].get_data()[1][np.argmin(abs(y[lat][0][0].get_data()[0] - self.m.urcrnrx))] for lat in y.keys()]
+        xtpts = [x[lat][0][0].get_data()[0][np.argmin(abs(x[lat][0][0].get_data()[1] - 0))] for lat in x.keys()]
+        xbpts = [x[lat][0][0].get_data()[0][np.argmin(abs(x[lat][0][0].get_data()[1] - self.m.urcrnry))] for lat in x.keys()]
+
+        # now plot the ticks as a scatter
+        self.m.scatter(len(ylpts)*[0], ylpts, 100, c='k', marker='_', linewidths=1, zorder=100)
+        self.m.scatter(len(yrpts)*[self.m.urcrnrx], yrpts, 100, c='k', marker='_', linewidths=1, zorder=100)
+        self.m.scatter(xtpts, len(xtpts)*[0], 100, c='k', marker='|', linewidths=1, zorder=100)
+        self.m.scatter(xbpts, len(xbpts)*[self.m.urcrnry], 100, c='k', marker='|', linewidths=1, zorder=100)
+
+    def add_scalebar(self, scalebar_params, loc=None, color='k', scalebar_pad=0.08):
+
+        m, ax = self.m, self.ax
+        previousfontfamily = plt.rcParams['font.family']
+        plt.rcParams['font.family'] = plt.rcParams['font.family'] = 'Univers 57 Condensed'
+
+        # scalebar properties
+        units = ['Miles', 'Kilometers']
+        scalebar_length = scalebar_params['length'] # miles
+        major_tick_interval = scalebar_params['major_tick_interval']
+        multiplier = 1 * 5280 * 0.3048 # to convert scalebar length to map coordinate units
+        multiplier2 = 0.621371 # multiplier to go from top units to bottom units (e.g. mi per km)
+        scalebar_length_mu = scalebar_length * multiplier
+
+        # scalebar position
+        if loc == 'inside_upper_right':
+            axis_pad = scalebar_pad * m.urcrnrx
+            position = [m.urcrnrx - axis_pad * np.sqrt(scalebar_params['fontsize']/6.0) - scalebar_length_mu, m.urcrnry - 0.8*axis_pad]
+        elif loc == 'inside_lower_right':
+            axis_pad = scalebar_pad * m.urcrnrx
+            position = [m.urcrnrx - axis_pad * np.sqrt(scalebar_params['fontsize']/6.0) - scalebar_length_mu, 0 + axis_pad]
+        elif loc == 'outside_lower_right':
+            axis_pad = scalebar_pad * m.urcrnrx
+            position = [m.urcrnrx - axis_pad * np.sqrt(scalebar_params['fontsize']/6.0) - scalebar_length_mu, 0 - 0.1 * m.urcrnrx]
+        elif loc == 'grid_outside_lower_right':
+            axis_pad = scalebar_pad * m.urcrnrx
+            position = [m.urcrnrx - axis_pad * np.sqrt(scalebar_params['fontsize']/6.0), 0 - axis_pad]
+        elif loc == 'centered':
+            axis_pad = 0.6 * scalebar_pad * m.urcrnrx
+            position = [0.55 * (m.urcrnrx - scalebar_length_mu), 0 - axis_pad]
+        elif loc == 'offcenter':
+            axis_pad = 0.6 * scalebar_pad * m.urcrnrx
+            position = [0.65 * (m.urcrnrx - scalebar_length_mu), 0 - axis_pad]
+        else:
+            axis_pad = scalebar_pad * m.urcrnrx
+            position = [m.urcrnrx + axis_pad, 0 + axis_pad]
+
+        # add bar line
+        x = [position[0], position[0] + scalebar_length_mu]
+        y = 2*[position[1]]
+        sb = plt.Line2D(x, y, color=color, zorder=99, lw=scalebar_params['lineweight'])
+        sb.set_clip_on(False)
+        ax.add_line(sb)
+
+        # setup position and length of ticks
+        ticks = np.arange(0,scalebar_length+major_tick_interval,major_tick_interval)
+        tick_positions = np.array([ticks * multiplier + position[0], ticks * multiplier * multiplier2 + position[0]])
+        tick_length = 0.05 * (tick_positions[0][-1] - tick_positions[0][0])
+        y = np.array([[y[0], y[0] + tick_length], [y[0], y[0] - tick_length]])
+
+        # convert over to axes fraction, for some reason mpl won't do 'data' coordinates
+        # for annotations outside plot area
+        tick_positions_ax = tick_positions/(m.urcrnrx)
+        tick_length_ax = tick_length/(m.urcrnry)
+        y_ax = y/(m.urcrnry)
+        valignments = ['bottom','top']
+        textpad = [0.5 * tick_length_ax, -0.5 * tick_length_ax] # 0 means that text is right on tick line
+
+        # for each unit, then for each tick
+        for n in range(2):
+            for i in range(len(ticks)):
+
+                # set ticks
+                l = plt.Line2D(2*[tick_positions[n][i]], y[n], color=color,
+                zorder=99, lw=scalebar_params['lineweight'])
+                l.set_clip_on(False)
+                ax.add_line(l)
+
+                # add tick labels
+                ax.annotate('{}'.format(ticks[i]), xy=(tick_positions_ax[n][i], y_ax[n][1]+textpad[n]),
+                xycoords='axes fraction', fontsize=scalebar_params['fontsize'], horizontalalignment='center',
+                color=color, verticalalignment=valignments[n])
+
+            # add end l(unit) abels
+            ax.annotate('    {}'.format(units[n]), xy=(tick_positions_ax[n][-1], y_ax[n][1]+textpad[n]),
+            xycoords='axes fraction', fontsize=scalebar_params['fontsize'], horizontalalignment='left',
+            color=color, verticalalignment=valignments[n])
+
+        plt.rcParams['font.family'] = previousfontfamily
